b90b4edacb1205fbd5986051beb94442d47459cc
coder95 <peter.li.feng@gmail.com>
2021-03-21 16:01:42 -0400

    Update CommitteeController.cs

--- QUDMMS_API/Controllers/CommitteeController.cs
+++ QUDMMS_API/Controllers/CommitteeController.cs
@@ -15,9 +15,9 @@ namespace QUDMMSAPI.Controllers
 {
     [Route("[controller]/[action]")]
     [ApiController]
-    public class AdminController : ControllerBase
+    public class CommitteeController : ControllerBase
     {
-        #region[QUDMMS] - Admin - TAMS - InstructorMS
+        #region[QUDMMS] - Admin - Committees
         [HttpPost]
         public async Task<ActionResult> BrowseInstructorList(JObject Parameter)
         {
@@ -29,10 +29,10 @@ namespace QUDMMSAPI.Controllers
                 }
 
                 string Sql = "";
-                DynamicParameters DyParam_InstructorList = new DynamicParameters();
+                DynamicParameters DyParam_CommitteeList = new DynamicParameters();
 
                 /*Dynamic Parameter Alter*/
-
+                /*
                 if (!string.IsNullOrEmpty(Convert.ToString(Parameter["name"])))
                 {
                     string Full_name = Convert.ToString(Parameter["name"]);
@@ -51,15 +51,40 @@ namespace QUDMMSAPI.Controllers
                     string instructor_id = Convert.ToString(Parameter["instructor_id"]);
                     Sql += "AND `instructor_id` = @instructor_id ";
                 }
+                */
 
+                /*Dynamic Parameter Alter*/
+                /*
+                if (!string.IsNullOrEmpty(Convert.ToString(Parameter["name"])))
+                {
+                    string Full_name = Convert.ToString(Parameter["name"]);
+                    var Names = Full_name.Split(' ', '.', ',');
+                    string First_name = Names[0];
+                    string Last_name = Names[1];
+
+                    Sql += "AND `first_name` = @first_name AND `last_name` = @last_name" + " ";
+
+                    DyParam_InstructorList.Add("first_name", First_name);
+                    DyParam_InstructorList.Add("last_name", Last_name);
+                }
+
+                if (!string.IsNullOrEmpty(Convert.ToString(Parameter["instructor_id"])))
+                {
+                    string instructor_id = Convert.ToString(Parameter["instructor_id"]);
+                    Sql += "AND `instructor_id` = @instructor_id ";
+                }
+                
+
+                /*
                 if (Sql != "") { Sql = "WHERE 1 = 1 " + Sql; }
 
-                /*calculate the total amount of records*/
                 string Original_zq61H4SJdL = XMLHelper.GetSql("SQ_zq61H4SJdL");
                 Original_zq61H4SJdL = Original_zq61H4SJdL.Replace("@dynamic_condition", Sql);
                 DataTable DT_zq61H4SJdL = await DapperHelper.ExecuteSqlDataTableAsync(Original_zq61H4SJdL, DyParam_InstructorList);
 
                 /*Get pagination */
+
+                /*
                 DyParam_InstructorList.Add("page_number", (Convert.ToInt32(Parameter["page_number"]) - 1) * 10);
                 string Original_U83Eb0bl4Q = XMLHelper.GetSql("SQL_U83Eb0bl4Q");
                 Original_U83Eb0bl4Q = Original_U83Eb0bl4Q.Replace("@dynamic_condition", Sql);
@@ -99,28 +124,71 @@ namespace QUDMMSAPI.Controllers
                 return BadRequest("Unknow Error");
             }
         }
-
+        */ 
         [HttpPost]
-        public async Task<ActionResult> CreateInstructor(JObject Parameter)
+        public async Task<ActionResult> SearchCommittee(JObject Parameter)
         {
 
-            Object Param_gdUg2TfBXJ = new
+            Object Param_blahblahblah = new
+            try
             {
-                instructor_id = Convert.ToString(Parameter["instructor_id"]),
-                title = Convert.ToString(Parameter["title"]),
-                first_name = Convert.ToString(Parameter["first_name"]),
-                last_name = Convert.ToString(Parameter["last_name"]),
-                email = Convert.ToString(Parameter["email"]),
-                years_exp = Convert.ToString(Parameter["years_exp"])
-            };
-            //string aa = " INSERT INTO `QUDMMS_DB`.`QUDM_TAMS_instructorInfo`(`instructor_id`,`title`,`first_name`,`last_name`,`email`,`years_exp`)VALUE(@instructor_id,@title,@first_name,@last_name,@email,@years_exp); ";
+                object Param_commsSearch = new
+                {
+                    committee_id = Convert.ToString(Parameter["committee_id"])
+                };
 
-            await DapperHelper.ExecuteSqlIntAsync(XMLHelper.GetSql("SQL_gdUg2TfBXJ"), Param_gdUg2TfBXJ);
-
-            return Ok("Create instructor profile success.");
-
+                DataTable DT_Param_commsSearch = await DapperHelper.ExecuteSqlDataTableAsync(XMLHelper.GetSql("Param_commsSearch"), Param_commsSearch);
+                JObject Json_Param_commsSearch = (JObject)JArray.Parse(JsonConvert.SerializeObject(Param_commsSearch))[0];
+                JObject Json_Result = Param_commsSearch;
+                return Ok(Json_Result);
+            }
+            catch (Exception ex) { return BadRequest("Load Committee Detail Failed."); }
+           
         }
 
+        [HttpPost]
+        public async Task<ActionResult> CreateCommittee(JObject Parameter)
+        {
+
+            Object Param_comms = new
+            {
+                cttee_id = Convert.ToString(Parameter["cttee_id"]),
+                cttee_tittle = Convert.ToString(Parameter["cttee_tittle"]),
+                chair_admin_weight = Convert.ToString(Parameter["chair_admin_weight"]),
+                co_chair_admin_weight = Convert.ToString(Parameter["co_chair_admin_weight"]),
+                secretary_admin_weight = Convert.ToString(Parameter["secretary_admin_weight"]),
+                memeber_admin_weight = Convert.ToString(Parameter["memeber_admin_weight"]),
+                cttee_notes = Convert.ToString(Parameter["cttee_notes"]),
+            };
+            await DapperHelper.ExecuteSqlIntAsync(XMLHelper.GetSql("SQL_comms"), Param_comms);
+
+            return Ok("Successfully created committee.");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteCommittee(JObject Parameter)
+        {
+            try
+            {
+                Object Param_CommsDel = new
+                {
+                    committee_id = Convert.ToString(Parameter["committee_id"])
+                };
+
+                await DapperHelper.ExecuteSqlIntAsync(XMLHelper.GetSql("SQL_CommsDel"), Param_CommsDel);
+                return Ok("Committee was successfully removed");
+            }
+            catch (Exception ex) { return BadRequest("Committee removal was unsuccessful"); }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateCommittee(JObject Parameter)
+        {
+            try { return Ok(""); } catch (Exception ex) { return BadRequest("UpdateCommiteeFailed"); }
+        }
+        #endregion
+
+        /*
         [HttpPost]
         public async Task<ActionResult> InstructorDetail(JObject Parameter)
         {
@@ -138,28 +206,7 @@ namespace QUDMMSAPI.Controllers
             }
             catch (Exception ex) { return BadRequest("Load Instructor Detail Failed."); }
         }
-
-        [HttpPost]
-        public async Task<ActionResult> DeleteInstructor(JObject Parameter)
-        {
-            try
-            {
-                Object Param_2fufaYyDsT = new
-                {
-                    instructor_id = Convert.ToString(Parameter["instructor_id"])
-                };
-
-                await DapperHelper.ExecuteSqlIntAsync(XMLHelper.GetSql("SQL_2fufaYyDsT"), Param_2fufaYyDsT);
-                return Ok("Instructor profile delted");
-            }
-            catch (Exception ex) { return BadRequest("Delete instructor profile Failed"); }
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> UpdateInstructor(JObject Parameter)
-        {
-            try { return Ok(""); } catch (Exception ex) { return BadRequest("UpdateInstructorFailed"); }
-        }
-        #endregion
+        */
+        
     }
 }
